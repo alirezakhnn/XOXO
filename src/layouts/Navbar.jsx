4683fd35476a379b83f8fe707eb54d0cab462f00
alireza <alirezakhanmohammad2002@gmail.com>
2022-09-17 15:30:52 +0430

    navbar has been responsived

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,33 +1,54 @@
-
+import { useState, Fragment } from 'react';
 import '../css/navbar.css';
 import { Input } from '../components';
 import { Link } from 'react-router-dom';
-import { profile_nina } from '../assets';
+import { profile_nina, menu, close } from '../assets';
+import Users from '../styles';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     return (
-        <section id="navbar">
-            <div className="left-navbar">
+        <Fragment>
+            <div className="hamburger-menu">
+                <img src={toggle ? close : menu} alt="hamburger"
+                    onClick={() => {
+                        setToggle(prev => !prev);
+                        document.body.classList.toggle('mobile-navbar-appear');
+                    }}
+                />
+            </div>
+            <div className=" mobile-left-navbar left-navbar">
                 <div className="logo">
                     <h1><Link id="xoxo" to="/">XOXO</Link></h1>
                 </div>
                 <div className="profile">
                     <img id="user" src={profile_nina} alt="profile" />
-                    <h3 id="username">Nina</h3>
+                    <h3 id="username">{Users[2].username}</h3>
                 </div>
             </div>
-            <div className="right-navbar">
-                <ul className="right-navbar-list">
-                    <li><Link to="/your-panel" id="panel">Panel</Link></li>
-                    <li><Link to="/menu" id="menu">Menu</Link></li>
-                    <li><Link to="/about" id="about">About</Link></li>
-                </ul>
-                <div className="sign">
-                    <h2 id="signin">signIn</h2>
-                    <Input id="signup" type="submit" value="signUp" />
+            <section id="navbar">
+                <div className="left-navbar">
+                    <div className="logo">
+                        <h1><Link id="xoxo" to="/">XOXO</Link></h1>
+                    </div>
+                    <div className="profile">
+                        <img id="user" src={profile_nina} alt="profile" />
+                        <h3 id="username">Nina</h3>
+                    </div>
                 </div>
-            </div>
-        </section>
+                <div className="right-navbar">
+                    <ul className="right-navbar-list">
+                        <li><Link to="/your-panel" id="panel">Panel</Link></li>
+                        <li><Link to="/menu" id="menu">Menu</Link></li>
+                        <li><Link to="/about" id="about">About</Link></li>
+                    </ul>
+                    <div className="sign">
+                        <h2 id="signin">signIn</h2>
+                        <Input id="signup" type="submit" value="signUp" />
+                    </div>
+                </div>
+            </section>
+        </Fragment>
     );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,38 @@
+    import { 
+    frier,
+    john,
+    profile_nina,
+    lagertha,
+    robert,
+} from './assets';
+
+
+const Users = [
+    {
+        id:'frier',
+        username:'Frier',
+        img:frier
+    },
+    {
+        id:'john',
+        name:'John',
+        img:john
+    },
+    {
+        id:'profile_nina',
+        username:'Nina',
+        img:profile_nina
+    },
+    {
+        id:'lagertha',
+        username:'Lagertha',
+        img:lagertha
+    },
+    {
+        id:'robert',
+        username:'Robert',
+        img:robert
+    }
+]
+
+export default Users
