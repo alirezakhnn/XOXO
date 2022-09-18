c97138ee129ffcb7adc9d6984a0622c496fb362d
alireza <alirezakhanmohammad2002@gmail.com>
2022-09-18 11:11:51 +0430

    header responsived

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,24 +1,23 @@
 import '../css/header.css';
 import Texts from '../texts';
-const Header = () => {
-    return (
-        <header id='header'>
-            <div className="right-header">
-                <h2 className="right-header-title">
-                    A Chatbox <br /> for instant <br /> messages <br /> or something <br /> like that
-                </h2>
-                <p className="right-header-context">{Texts[0].header.right.context}</p>
+const Header = () => (
+    <header id='header'>
+        <div className="left-header">
+            <h2 className="left-header-title">
+                A Chatbox <br /> for instant <br /> messages <br /> or something <br /> like that
+            </h2>
+            <p className="left-header-context">{Texts[0].header.right.context}</p>
+        </div>
+        <div className="right-header">
+            <div className="right-header-first">
+                <p className='right-header-first-context'>{Texts[0].header.right.above}</p>
             </div>
-            <div className="left-header">
-                <div className="left-header-first">
-                    <p className='left-header-first-context'>{Texts[0].header.right.above}</p>
-                </div>
-                <div className="left-header-second">
-                    <p className="left-header-second-context">{Texts[0].header.right.below}</p>
-                </div>
+            <div className="right-header-second">
+                <p className="right-header-second-context">{Texts[0].header.right.below}</p>
             </div>
-        </header>
-    );
-}
+        </div>
+    </header>
+);
+
 
 export default Header
--- src/texts.js
+++ src/texts.js
@@ -13,13 +13,13 @@ const Texts = [
             },
             right: {
                 above:'Google Fonts is a library of 1,451 open source font families and APIs for convenient use via CSS and Android.The library also has delightful and beautifully crafted icons for common actions and items.Download them for use in your digital products for Android, iOS, and web.',
-                below:'Google Fonts is a library of 1,451 open source font families and APIs for convenient use via CSS and Android.The library also has delightful and beautifully crafted icons for common actions and items.Download them for use in your digital products for Android, iOS, and web.',
+                below:'Google Fonts is a library of 1,451 open source font families and APIs for convenient use via CSS and Android.',
                 context:'Discover new web Application'
             }
         },
         sign : {
-            in:'signIn',
-            up:'signUp'
+            in:'SignIn',
+            up:'SignUp'
         }
     }
 ];
