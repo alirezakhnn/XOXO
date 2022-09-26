c1ac067688902bf189ca65f845d487c8314296d7
alireza <alirezakhanmohammad2002@gmail.com>
2022-09-26 15:06:24 +0330

    videos added

---
+++ src/videos/index.js
@@ -0,0 +1,11 @@
+import Chat from './chat.mp4';
+import Space from './space.mp4';
+import Tv from './tv.mp4';
+import XOXO_com from './xoxo-comminucation.mp4';
+
+export {
+    Chat,
+    Space,
+    Tv,
+    XOXO_com
+}
